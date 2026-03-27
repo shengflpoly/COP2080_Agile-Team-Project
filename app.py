fa07619b990788048e776c2d160aa8eb63ede87a
shen <she6510@floridapoly.edu>
2026-03-27 18:52:00 +0000

    Edited app.py input

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import streamlit as st
 import pandas as pd
+
+st.sidebar.subheader("Entry Form")
+name = st.sidebar.text_input("Student Name")
+score = st.sidebar.number_input("Score", min_value=0, max_value=100, step=1)
+
+if (st.sidebar.button("Add Student")):
+    data = [name, score]
+
