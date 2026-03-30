3537824a5fdf666be16193f58032c45040e15847
shen <she6510@floridapoly.edu>
2026-03-30 18:22:30 +0000

    display edited

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Task B input
 import streamlit as st
 import pandas as pd
 
@@ -8,3 +9,11 @@ score = st.sidebar.number_input("Score", min_value=0, max_value=100, step=1)
 if (st.sidebar.button("Add Student")):
     data = [name, score]
 
+#Task C display
+#from logic import ___
+st.header("Faculty Grade Dashboard")
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Average", avg)
+col2.metric("Highest", high)
+col3.metric("Lowest", low)
