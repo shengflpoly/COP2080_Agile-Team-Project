f355fb6294163095dc1e4bbd5f61f7b3535cb8b0
Cayden Mclendon <cmclendon2223@floridapoly.edu>
2026-03-30 18:20:29 +0000

    logic modual

--- logic.py
+++ logic.py
@@ -1,9 +1,22 @@
 import pandas as pd
-Students = pd.DataFrame(columns=["Name", "Score", "Grade"])
+__Students = pd.DataFrame(columns=["Name", "Score", "Grade"])
+__Average = 0
+__Max = 0
+__Min = 0
 
-Average = 0
-Max = 0
-Min = 0
+def __UpdateScore():
+    tSum = 0
+    tMin = 999
+    tMax = 0
+    rowNum = 0
+    for index, row in __Students.iterrows():
+        tSum += row["Score"]
+        if (row["Score"] < tMin):
+            tMin = row["Score"]
+        if (row["Score"] > tMax):
+            tMax = row["Score"]
+        rowNum += 1
+    return (tSum / rowNum, tMin, tMax)
 
 def AddStudent(Name, Score):
     Letter = ""
@@ -14,29 +27,25 @@ def AddStudent(Name, Score):
     else: Letter = "F"
 
     row = [Name, Score, Letter]
-    Students.loc[len(Students)] = row
-    Average, Min, Max = __UpdateScore()
+    __Students.loc[len(__Students)] = row
 
-def __UpdateScore():
-    tSum = 0
-    tMin = 999
-    tMax = 0
-    rowNum = 0
-    for index, row in Students.iterrows():
-        tSum += row["Score"]
-        if (row["Score"] < tMin):
-            tMin = row["Score"]
-        if (row["Score"] > tMax):
-            tMax = row["Score"]
-        rowNum += 1
-    return (tSum / rowNum, tMin, tMax)
-    
+    global __Average, __Min, __Max
+    (__Average, __Min, __Max) = __UpdateScore()
 
+def GetDataframe():
+    return __Students
+
+def GetMin():
+    return __Min
+
+def GetMax():
+    return __Max
+
+def GetAverage():
+    return __Average
 
 
-AddStudent("bob", 85)
-AddStudent("cam", 40)
-print(Average)
+
         
 
 
